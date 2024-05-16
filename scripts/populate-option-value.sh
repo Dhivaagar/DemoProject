f69535f1e8e2258ca97f21b408ffc296426116c9
dhivaagar <dhivaagar02@gmail.com>
2024-05-16 15:22:12 +0530

    push local code to master

--- scripts/populate-option-value.sh
+++ scripts/populate-option-value.sh
@@ -1,50 +1,26 @@
 #!/bin/bash
 
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(dirname "$0")
 SOURCE_YAML_FILE="$SCRIPT_DIR/../.github/workflows/target-action.yml"
 
-
-# dynamically generate values.
-# ex. could read repository or file to get values
+# Define options
 OPTIONS=(major minor patch prerelease)
 
+# Read current options from YAML file
+current_options=$(yq eval '.on.workflow_dispatch.inputs.version.options' "$SOURCE_YAML_FILE")
 
-current_options=$(yq eval '.on.workflow_dispatch.inputs.version.options' $SOURCE_YAML_FILE )
-
-
-current_options_array=()
-while read -r word; do
-    current_options_array+=("$word")
-done <<< "$current_options"
-
-
-declare -a output_array=()
-
-
-# shellcheck disable=SC2128
-for i in $OPTIONS; do
-    output_array+=("$i")
-done
-
+# Convert current options string to array
+IFS=', ' read -r -a current_options_array <<< "$current_options"
 
 # Check if the values in the arrays are equal
-if [[ "${current_options_array[*]}" == "${output_array[*]}" ]]; then
+if [[ "${current_options_array[*]}" == "${OPTIONS[*]}" ]]; then
     echo "Values in YAML file are equal to values in array. No update needed."
 else
     echo "Values in YAML file are not equal to values in array. Updating YAML file."
     # Construct YAML-compatible string
-    options_string="["
+    options_string=$(printf '"%s", ' "${OPTIONS[@]}")
+    options_string="[${options_string%, }]"
 
-
-    for option in "${output_array[@]}"; do
-        options_string+="\\"$option\\", "
-    done
-
-
-    options_string="${options_string%, }]"
-
-
-    yq eval ".on.workflow_dispatch.inputs.version.options = $options_string" $SOURCE_YAML_FILE > temp.yml && mv temp.yml $SOURCE_YAML_FILE
+    # Update YAML file using yq
+    yq eval ".on.workflow_dispatch.inputs.version.options = $options_string" "$SOURCE_YAML_FILE" -i
 fi
-
