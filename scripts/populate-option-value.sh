390f2529e139e68e10227be0a6fcda9341c5b7e2
dhivaagar <dhivaagar02@gmail.com>
2024-05-16 17:18:17 +0530

    push local code to master

--- scripts/populate-option-value.sh
+++ scripts/populate-option-value.sh
@@ -1,24 +1,34 @@
-SCRIPT_DIR=$(dirname "$0")
-SOURCE_YAML_FILE="$SCRIPT_DIR/../.github/workflows/target-action.yml"
+#!/usr/bin/bash
 
-# Define options
 OPTIONS=(major minor patch prerelease)
 
-# Read current options from YAML file
-current_options=$(yq eval '.on.workflow_dispatch.inputs.version.options' "$SOURCE_YAML_FILE")
+current_options=$(yq eval '.on.workflow_dispatch.inputs.version.options' "./.github/workflows/target-action.yml")
 
-# Convert current options string to array
-IFS=', ' read -r -a current_options_array <<< "$current_options"
+current_options_array=()
+while read -r word; do
+    current_options_array+=("$word")
+done <<< "$current_options"
+
+declare -a output_array=()
+
+# shellcheck disable=SC2128
+for i in $OPTIONS; do
+    output_array+=("$i")
+done
 
 # Check if the values in the arrays are equal
-if [[ "${current_options_array[*]}" == "${OPTIONS[*]}" ]]; then
+if [[ "${current_options_array[*]}" == "${output_array[*]}" ]]; then
     echo "Values in YAML file are equal to values in array. No update needed."
 else
     echo "Values in YAML file are not equal to values in array. Updating YAML file."
     # Construct YAML-compatible string
-    options_string=$(printf '"%s", ' "${OPTIONS[@]}")
-    options_string="[${options_string%, }]"
+    options_string="["
 
-    # Update YAML file using yq
-    yq eval ".on.workflow_dispatch.inputs.version.options = $options_string" "$SOURCE_YAML_FILE" -i
+    for option in "${output_array[@]}"; do
+        options_string+="\\"$option\\", "
+    done
+
+    options_string="${options_string%, }]"
+
+    yq eval ".on.workflow_dispatch.inputs.version.options = $options_string" "./.github/workflows/target-action.yml" > temp.yml && mv temp.yml "./.github/workflows/target-action.yml"
 fi
